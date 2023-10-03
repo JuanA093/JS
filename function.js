da2ade4bef40058ea7e146d8c39ae47eed4766dc
JuanA093 <andrade.juan0903@gmail.com>
2023-10-02 23:40:17 -0500

    Cambios Finales

--- function.js
+++ function.js
@@ -3,7 +3,7 @@ const txtNombre = document.getElementById("txtNombre");
 const nbCantidad = document.getElementById("nbCantidad");
 const nbPrecio = document.getElementById("nbPrecio");
 const txtDescripcion = document.getElementById("txtDescripcion");
-const tabla = document.getElementById("contendidoTabla");
+const tabla = document.getElementById("contenidoTabla");
 const eliminardatos = document.getElementById("limpiar");
 
 const datos = [];
@@ -19,25 +19,30 @@ function agregarElemento(){
 
     console.log(datos);
 
-    let tabla = document.getElementById("contenidoTabla");
-    tabla.innerHTML += "<tr><td>" + id + "</td><td>" + nombre + "</td><td>" + cantidad + "</td  ><td>" + precio + "</td><td>" + descripcion + "</td></tr>";
+    let idFila = "fila" + datos.length;
 
+    let tabla = document.getElementById("contenidoTabla");
+    tabla.innerHTML += "<tr id = '" + idFila + "'><td>" + id + "</td><td>" + nombre + "</td><td>" + cantidad + "</td><td>" + precio + "</td><td>" + descripcion + "</td><td><button class ='eliminar' onclick='eliminarFila(\""+ idFila + "\")'>Eliminar</button></td></tr>";
+
+    //borrar datos del form al agregar a la tabla
     document.getElementById("txtId").value = "";
     document.getElementById("txtNombre").value = "";
     document.getElementById("nbCantidad").value = "";
     document.getElementById("nbPrecio").value = "";
     document.getElementById("txtDescripcion").value = "";
 
-    function limpiarDatos(){
-        tabla.innerHTML = "";
-    }
+}    
 
-
-    let eliminar = document.getElementById("limpiar");
-    eliminar.addEventListener('click',limpiarDatos);
-
+function eliminarFila(idFila){
+        
+        let fila = document.getElementById(idFila);
+        fila.remove();
 
 }
+ 
+
+
+
 
 
 
